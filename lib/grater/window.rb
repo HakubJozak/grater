9cca78a58cf512a46c7df6c2b36bd35d8fb209d3
Jakub Hozak <jakub.hozak@gmail.com>
2015-12-26 00:34:10 +0100

    + functions

--- lib/grater/window.rb
+++ lib/grater/window.rb
@@ -13,38 +13,63 @@ class Grater::Window
       new(r.strip)
     end
   end
-  
-  def name
-    wmctrl '-lx'
-  end
 
+  def self.active
+    out = `wmctrl -a :ACTIVE: -v 2>&1`
+    id = out.split('Using window: ').last
+    new(id)
+  end
+  
   def to_dmenu
     Dmenu::Item.new(@title)
   end
 
   def activate
-    wmctrl "a"
+    wmctrl "-a #{@id}"
   end
 
   def summon
-    wmctrl "R"
+    wmctrl "-R #{@id}"
+  end
+
+  def maximize
+    wmctrl "-r #{@id} -b add,maximized_vert,maximized_horz"
+  end
+
+  def unmaximize
+    wmctrl "-r #{@id} -b remove,maximized_vert,maximized_horz"
+  end
+
+  def maximize_x
+    wmctrl "-r #{@id} -b add,maximized_horz"
+  end
+
+  def maximize_y
+    wmctrl "-r #{@id} -b add,maximized_vert"
+  end
+
+  def shade
+    wmctrl "-r #{@id} -b add,shaded"
+  end
+
+  def unshade
+    wmctrl "-r #{@id} -b remove,shaded"
   end
 
   def close
-    wmctrl "c"
+    wmctrl "-c #{@id}"
   end
 
   private
+
+  def wmctrl(switches)
+    run "wmctrl -i #{switches}"
+    self
+  end
 
   def run(cmd)
     puts cmd
     `#{cmd}`.strip
   end
-
-  def wmctrl(switches)
-    cmd "wmctrl -i #{switches} #{@id}"
-    self
-  end
-
 
 end
